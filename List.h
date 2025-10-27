7bf13c14e825e39c43e83471faaa5eecdb28dba4
DDUNCA <ddunca@alumno.upv.es>
2025-10-27 19:28:20 +0100

    List.h arreglado

--- List.h
+++ List.h
@@ -9,7 +9,7 @@ public:
     virtual bool isEmpty() const = 0;
     virtual int size() const = 0;
     virtual void insert(int index, const T& element) = 0;
-    virtual void remove(int index) = 0;
+    virtual T&  remove(int index) = 0;
     virtual T& get(int index) = 0;
     virtual void clear() = 0;
 };
